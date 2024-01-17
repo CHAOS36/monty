fa096dbbb3147c8593aa56750651741a14cbcfa8
CHAOS36 <badrelhoudni96@gmail.com>
2024-01-17 06:22:56 -0800

    .

---
+++ monty_operators2.c
@@ -0,0 +1,39 @@
+#include "monty.h"
+
+/**
+* f_push - function that adds node to the stack
+* @head: double head pointer to the stack
+* @counter: line count
+*
+* Return: nothing
+*/
+void f_push(stack_t **head, unsigned int counter)
+{
+	int i, p = 0, flaged = 0;
+
+	if (bus.arg)
+	{
+		if (bus.arg[0] == '-')
+			p++;
+		for (; bus.arg[p] != '\0'; p++)
+		{
+			if (bus.arg[p] > 57 || bus.arg[p] < 48)
+				flag = 1; }
+		if (flaged == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	i = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, i);
+	else
+		addqueue(head, i);
+}
